edd8d0e775698572304fa85e469144096b22ebd2
KishorSura <117828152+KishorSura@users.noreply.github.com>
2023-01-23 13:14:40 +0530

    Update Window function.sql

--- Window function.sql
+++ Window function.sql
@@ -1,10 +1,14 @@
+-- A database is created
 create database windowsfunction;
 use windowsfunction; 
+
+-- creating table name employee
 create table employee (
 Name	varchar(20),
 Age int,
-	Department varchar(20),
-	Salary int);
+Department varchar(20),
+Salary int);
+-- inserting values
  insert into employee (Name,age,department,salary)
  values 
  ("Ramesh",	20,	"Finance"	,50000),
@@ -14,24 +18,20 @@ Age int,
 ("Pradeep"	,22,	"Sales"	,20000) ;
 
 
--- avg
+-- avg partition by department and order by age 
 select name,age,department,salary,avg(salary) over (partition by department order by age ) as avg_salary from employee;
+-- avg partition by department and
 select name,age,department,salary,avg(salary) over (partition by department) as avg_salary from employee; 
 
-
+-- using of dense_rank,row_number,rank 
 select name,age,department,salary, rank() over (partition by department order by salary desc) as empl_rank ,
 dense_rank() over (partition by department order by salary desc) as empl_denserank,
 row_number() over(partition by department order by salary  desc) as empl_row_number 
 from employee;
 
 
-select *,rank() over (partition by department order by salary desc) as empl_rank,
-dense_rank() over (partition by department order by salary desc) as empl_dense,
-row_number() over (partition by department order by salary desc)  as rrow_number 
-from employee; 
 
-
--- window function 
+-- The csv file is directly imported to work bench and basic window function queries are performed where partition by and order by is also used in arranging of rows 
 select * from details; 
 -- sum 
 select * ,sum(current_inventory) over() as total_sum from details; 
@@ -49,7 +49,7 @@ sum(current_inventory) over (partition by product_type order by product_id) as c
 sum(current_inventory) over (partition by product_type order by product_id rows between unbounded preceding and current row) as cum_sum2
 from details ;
 
-
+-- average
 select *,avg(current_inventory) over (partition by product_type order by product_id rows between unbounded preceding and unbounded following) as avg_ ,
 avg(current_inventory) over (partition by product_type order by product_id) as cum_avg ,
 avg(current_inventory) over (partition by product_type order by product_id rows between unbounded preceding and current row) as cum_avg2
